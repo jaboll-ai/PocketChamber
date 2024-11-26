91053640e5810c3c02636d2151ebb7fd6392e707
jaboll-ai <spexe0011@gmail.com>
2024-11-26 10:07:21 +0100

    still not working

---
+++ src/main/java/com/gmail/jaboll/mc/client/BEWLR.java
@@ -0,0 +1,52 @@
+package com.gmail.jaboll.mc.client;
+
+import com.gmail.jaboll.mc.PocketChamber;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.model.SkullModelBase;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.client.renderer.blockentity.SkullBlockRenderer;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.ResolvableProfile;
+import net.minecraft.world.level.block.PlayerHeadBlock;
+import net.minecraft.world.level.block.SkullBlock;
+
+import java.util.Map;
+
+public class BEWLR extends BlockEntityWithoutLevelRenderer {
+    private final EntityModelSet entityModelSet;
+    private Map<SkullBlock.Type, SkullModelBase> skullModels;
+
+    public BEWLR(BlockEntityRenderDispatcher blockEntityRenderDispatcher, EntityModelSet entityModelSet) {
+        super(blockEntityRenderDispatcher, entityModelSet);
+        this.entityModelSet = entityModelSet;
+    }
+
+
+    @Override
+    public void onResourceManagerReload(ResourceManager resourceManager) {
+        this.skullModels = SkullBlockRenderer.createSkullRenderers(this.entityModelSet);
+    }
+
+    @Override
+    public void renderByItem(ItemStack stack, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource buffer, int packedLight, int packedOverlay) {
+        if (Screen.hasShiftDown()){
+            ResolvableProfile resolvableProfile =  stack.getComponents().get(PocketChamber.PLAYER_PROFILE_COMPONENT.get());
+            PlayerHeadBlock playerHeadBlock = (PlayerHeadBlock) ((BlockItem) Items.PLAYER_HEAD).getBlock();
+            SkullModelBase skullmodelbase = this.skullModels.get(playerHeadBlock.getType());
+            RenderType rendertype = SkullBlockRenderer.getRenderType(playerHeadBlock.getType(), resolvableProfile);
+            SkullBlockRenderer.renderSkull(null, 180.0F, 0.0F, poseStack, buffer, packedLight, skullmodelbase, rendertype);
+        }
+        super.renderByItem(stack, displayContext, poseStack, buffer, packedLight, packedOverlay);
+    }
+
+}
--- src/main/java/com/gmail/jaboll/mc/client/Client.java
+++ src/main/java/com/gmail/jaboll/mc/client/Client.java
@@ -1,6 +1,9 @@
 package com.gmail.jaboll.mc.client;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -9,7 +12,11 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.ModelEvent;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
+import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import net.neoforged.neoforge.client.model.BakedModelWrapper;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import com.gmail.jaboll.mc.blocks.particle.PocketChamberParticleProvider;
 
@@ -40,5 +47,32 @@ public class Client {
     @SubscribeEvent
     public static void registerParticleFactories(RegisterParticleProvidersEvent event){
         event.registerSpriteSet(STASIS_CHAMBER_PARTICLE.get(), PocketChamberParticleProvider::new);
+    }
+
+    @SubscribeEvent
+    public static void modifyBakingResult(ModelEvent.ModifyBakingResult event) {
+        event.getModels().computeIfPresent(
+                // The model resource location of the model to modify.
+                ModelResourceLocation.inventory(
+                        ResourceLocation.fromNamespaceAndPath("pocketchamber", "stasis_chamber")
+                ),
+                // A BiFunction with the location and the original models as parameters, returning the new model.
+                (location, model) -> new BakedModelWrapper<>(model) {
+                    @Override
+                    public boolean isCustomRenderer() {
+                        return true; // Ensure custom rendering
+                    }
+                }
+        );
+    }
+
+    @SubscribeEvent
+    public static void registerClientExtension(RegisterClientExtensionsEvent event){
+        event.registerItem(new IClientItemExtensions() {
+            @Override
+            public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                return new BEWLR(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels());
+            }
+        }, STASIS_CHAMBER_ITEM);
     }
 }
